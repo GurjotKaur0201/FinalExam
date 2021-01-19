fba4bf0de8b673bb73cb256734d3a001053a77e8
GurjotKaur0201 <grewa@LAPTOP-JVNP70QS>
2021-01-18 20:00:36 -0500

    event

---
+++ eventTest/eventTest/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace eventTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+        }
+        public event CalendarvisibilityEventHandler cal;
+        protected void calendar(object sender, CalendarvisiblitityEventArgs e)
+        {
+            if (cal != null)
+            {
+
+            }
+        }
+    }
+    
+    public class CalendarvisiblitityEventArgs: EventArgs
+	{
+		private bool _isCalendarVisible;
+
+		public bool IsCalendarVisible
+        {
+            get
+            {
+				return this._isCalendarVisible;
+            }
+        }
+        public CalendarvisiblitityEventArgs(bool isCalendarVisible)
+        {
+            this._isCalendarVisible = isCalendarVisible;
+        }
+
+	}
+    public delegate void CalendarvisibilityEventHandler(object sender, CalendarvisiblitityEventArgs e);
+
+
+	
+}
