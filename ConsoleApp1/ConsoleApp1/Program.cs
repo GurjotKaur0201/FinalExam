292c0a56df457b0b36ac4e2d1b62b8b630d47048
GurjotKaur0201 <grewa@LAPTOP-JVNP70QS>
2021-01-18 20:33:25 -0500

    event

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // calendarvisibilityEventArgs obj = new calendarvisibilityEventArgs(false);
+            
+        }
+        public event CalendarVisibilityEventHandler calevent;
+        protected virtual void Onchange()
+        {
+            if(calevent != null)
+            {
+                calendarvisibilityEventArgs obj = new calendarvisibilityEventArgs(false);
+                calevent(this,obj);
+            }
+        }
+        
+
+    }
+
+    public class calendarvisibilityEventArgs: EventArgs
+    {
+        private bool _isCalendarVisible;
+        public bool IsCalendarVisible
+        {
+            get
+            {
+                return this._isCalendarVisible;
+            }
+        }
+        public calendarvisibilityEventArgs(bool isCalendarVisible)
+        {
+            this._isCalendarVisible = isCalendarVisible;
+        }
+    }
+    public delegate void CalendarVisibilityEventHandler(object sender, calendarvisibilityEventArgs e);
+}
